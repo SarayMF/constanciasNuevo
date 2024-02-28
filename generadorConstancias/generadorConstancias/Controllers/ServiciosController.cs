4e58087978ac16121e0ad797a8b69b8c3249c815
Saray Madrigal <a.madrigal@kodiak.com.mx>
2024-02-28 12:16:49 -0600

    primer commit

---
+++ generadorConstancias/generadorConstancias/Controllers/ServiciosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace generadorConstancias.Controllers
+{
+    public class ServiciosController : Controller
+    {
+       public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult consultar()
+        {
+            return View();
+        }
+
+        public ActionResult capturar()
+        {
+            return View();
+        }
+    }
+}
